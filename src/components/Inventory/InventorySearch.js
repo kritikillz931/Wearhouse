d8e37067ab4ee4d57cd9eb2855c05bdbae0e5d6c
justin <justinadams758@gmail.com>
2021-06-21 12:58:46 -0500

    saving data from api successfully

--- src/components/Inventory/InventorySearch.js
+++ src/components/Inventory/InventorySearch.js
@@ -1,10 +1,45 @@
-import React, { useContext, useEffect } from "react"
-
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory, useParams, } from 'react-router-dom';
 import {Button } from "reactstrap"
 import {InventoryContext} from "./InventoryProvider"
 
 
 export const InventorySearch = ({searchResult}) => {
+  const {addInventory, updateInventory} = useContext(InventoryContext)
+  
+ 
+  //gets searchResult id from url route
+  const {inventoryId} = useParams();
+
+  //gives ability to control navigation
+  const history = useHistory();
+const userId = parseInt(localStorage.getItem("wearhouse_user"))
+  console.log(searchResult)
+
+    const handleSaveInventory = () => {
+        if (inventoryId){
+          //PUT - update
+          updateInventory({
+            userId: userId,
+            silhouette: searchResult.image.thumbnail,
+            brand: searchResult.brand,
+           name: searchResult.name,
+            marketValue: searchResult.estimatedMarketValue,
+
+          })
+          .then(() => history.push(`/Inventory`))
+        } else {
+          //POST - add
+          addInventory({
+            userId: userId,
+            silhouette: searchResult.image.thumbnail,
+            brand: searchResult.brand,
+           name: searchResult.name,
+            marketValue: searchResult.estimatedMarketValue
+          })
+          .then(() => history.push("/Inventory"))
+        }
+      }
 
   return (
     <>
@@ -15,7 +50,11 @@ Gender: {searchResult.gender}<br/>
 Release Year: {searchResult.releaseYear}<br/>
 Colorway: {searchResult.colorway}</p>
 <img src={searchResult.image.thumbnail}></img>
-<Button >Save</Button>
+<Button className="btn btn-primary" onClick={event => {
+  event.preventDefault()
+    handleSaveInventory()
+  
+}}>Save</Button>
     <hr/>  
     </>
   )
