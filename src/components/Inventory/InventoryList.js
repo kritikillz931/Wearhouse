f5e50d8c6dbf8e0bf89e5afebd99575fa30a4981
justin <justinadams758@gmail.com>
2021-06-30 13:53:02 -0500

    updated inventory list styling

--- src/components/Inventory/InventoryList.js
+++ src/components/Inventory/InventoryList.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react"
 import { InventoryContext } from "./InventoryProvider"
 import { useHistory } from "react-router-dom"
-import { Table, Button, Modal, ModalBody, ModalHeader, ModalFooter, Container, Row, } from 'reactstrap';
+import { Table, Button, Modal, ModalBody, ModalHeader, ModalFooter, Container, Row, Jumbotron, } from 'reactstrap';
 import "./Inventory.css"
 import { TotalPricePaid } from "./InventoryTotalPrice"
 import { TotalMarketPrice } from "./InventoryMarketPrice"
@@ -57,50 +57,72 @@ export const InventoryList = (props) => {
 
   return (
     <>
-      <div>
-        <section className="InventoryContainer">
-          <div ><Table dark><thead><tr><th>Silhouette</th><th>Brand</th><th>Name</th><th>Size(per)</th><th>Price(per)</th><th>Market Value(per)</th><th>Quantity</th><th>Actions</th></tr></thead><tbody>
+      <div className="InventoryContainer">
+        <Container fluid="md">
+          <Jumbotron fluid className="text-white" id="inventoryHeader">
+            <h1 className="display-5">Current Inventory</h1>
+          </Jumbotron>
+        
+          <Table responsive hover dark size="sm">
+            <thead>
+              <tr>
+                <th class="inventoryPicCol">Silhouette</th>
+                <th class="inventoryBrandCol">Brand</th>
+                <th class="inventoryNameCol">Name</th>
+                <th class="inventoryPriceCol">Price Paid</th>
+                <th class="inventoryMarketCol">Market Value</th>
+                <th class="inventorySizeCol">Size</th>
+                <th class="inventoryQtyCol">Qty</th>
+                <th class="inventoryActionsCol">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="inventoryTableBody">
             {
               filteredInventoryList.map(inventory => {
                 return (
-                  <tr key={inventory.id}>
-                    <td><img style={{ height: '100px', width: '100px' }} className="silhouetteImg" src={inventory.silhouette}></img></td><td className="prodInfo">{inventory.brand}</td><td className="prodInfo">{inventory.name}</td><td className="prodInfo">{inventory.size}</td><td className="prodInfo">{inventory.price}</td><td className="prodInfo">{inventory.marketValue}</td><td className="prodInfo">{inventory.quantity}</td><td><Button className="text-white" color="info" size="sm" style={{ height: '30px', width: '40px' }} onClick={(event) => {
-                      event.preventDefault()
-                      openEditModal(inventory.id)
-                    }}>edit</Button> <Button className="text-white" color="info" size="sm" style={{ height: '30px', width: '60px' }} onClick={() => handleRelease(inventory.id)}>Delete</Button></td>
+                  <tr key={inventory.id} id="inventoryTableRow" onClick={(event) => {
+                    event.preventDefault()
+                    openEditModal(inventory.id)
+                  }}>
+                    <td><img  id="silhouetteImg" src={inventory.silhouette}></img></td>
+                    <td id="inventoryBrand">{inventory.brand}</td>
+                    <td id="inventoryName">{inventory.name}</td>
+                    <td >${inventory.price} <br />/each</td>
+                    <td >${inventory.marketValue} <br />/each</td>
+                    <td >{inventory.size}</td>
+                    <td >{inventory.quantity}</td>
+                    <td>
+                      {/* <Button className="text-white" color="info" size="sm" style={{ height: '30px', width: '40px' }} onClick={(event) => {
+                        event.preventDefault()
+                        openEditModal(inventory.id)
+                      }}>Edit</Button>  */}
+                      <Button className="text-white" color="info" size="sm" style={{ height: '30px', width: '60px' }} onClick={() => handleRelease(inventory.id)}>Delete</Button></td>
                   </tr>
                 )
               })
             }
           </tbody></Table>
-          </div>
-        </section>
-            <Container className="totalsContainer">
-              <Row className="justify-content-between" xs="1" sm="2" md="4">
-            <TotalPricePaid inventoryList={inventoryList} />
-            <TotalMarketPrice inventoryList={inventoryList} />
-            <TotalQuantityAmount inventoryList={inventoryList} />
-                
-              </Row>
-            <Button className="text-white" size="sm" style={{ height: '30px', width: '125px' }} color="info" onClick={toggle} >
-              Add New
-              </Button>
+              <Container className="totalsContainer">
+                <Table className="totalsContainer" responsive hover dark size="md">
+                  <thead>
+                    <th>
+                    <TotalPricePaid inventoryList={inventoryList} />
+                    </th>
+                    <th>
+                    <TotalMarketPrice inventoryList={inventoryList} />
+                    </th>
+                    <th>
+                    <TotalQuantityAmount inventoryList={inventoryList} />
+                    </th>
+                  </thead>
+                  
+                </Table>
+                <Button className="text-white" size="lg" color="info" onClick={toggle} >
+                Add New
+                </Button>
+              </Container>
             </Container>
 
-
-
-
-
-          {/* <div className="totalsContainer">
-            <div className="text-white">
-              <div className="totalPrice">
-              </div>
-              <div className="totalMarket">
-              </div>
-              <div className="totalQuantity">
-              </div>
-            </div>
-          </div> */}
       </div>
 
       <Modal isOpen={modal} toggle={toggle} className={className}>
